73e3149e1e7dd54b76cdce71810df87cfc1df3e7
MarcoMercado <mmercad4@uncc.edu>
2020-09-11 23:42:47 -0400

    Added create feature to todo app

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,16 +1,28 @@
 $(document).ready(function () {
 
     //Toggle line through task 
-    $("li").on("click", function() {
+    $("ul").on("click","li", function() {
         $(this).toggleClass("completed");
     });
 
-    $("span").on("click", function(event){
+    //Delete Todos
+    $("ul").on("click","span", function(event){
         $(this).parent().fadeOut(500, function(){
             $(this).remove();
         });
-        
+
         event.stopPropagation();
     })
+
+    $("input[type='text']").on("keypress", function(event) {
+        //making sure keypress is enter key/code which is 13
+        if (event.which === 13){
+            //grabbing new todo from text
+            var todoText = $(this).val();
+            $(this).val("");
+            //create new li and add to ul
+            $("ul").append("<li><span>X</span> " + todoText + "</li>");
+        }
+    });
 
 });
