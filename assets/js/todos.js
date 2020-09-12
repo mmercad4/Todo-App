a0a9f4358467d7ae06facc3188347af259502989
MarcoMercado <mmercad4@uncc.edu>
2020-09-11 23:31:07 -0400

    added remove functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
 
+    //Toggle line through task 
     $("li").on("click", function() {
         $(this).toggleClass("completed");
     });
+
+    $("span").on("click", function(event){
+        $(this).parent().fadeOut(500, function(){
+            $(this).remove();
+        });
+        
+        event.stopPropagation();
+    })
 
 });
